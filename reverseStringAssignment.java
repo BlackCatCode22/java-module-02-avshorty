a90739b444babfce58ceed9ab9bcca1a8dd1e88d
avshorty <119147361+avshorty@users.noreply.github.com>
2025-02-04 11:30:57 -0800

    Add files via upload

---
+++ reverseStringAssignment.java
@@ -0,0 +1,15 @@
+//andres villarreal module 2 assignment reverse string.
+import java.util.Scanner;
+public class reverseStringAssignment {
+    public static void main(String[] args) {
+        Scanner scanned = new Scanner(System.in);
+
+        System.out.println("I am your magic word genie watch me turn your words into a backwards mess");
+        System.out.println("ENTER YOUR WORD/SENTENCE");
+        String userWord = scanned.nextLine();
+        String reversed = new StringBuilder(userWord).reverse().toString();
+        System.out.println("Your reverse word/sentecne is"+reversed);
+
+
+    }
+}
